49bbd8539208ab14491900449757f921e2940b85
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-06-01 01:15:45 +0530

    config file

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-export const nftaddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
-export const nftmarketaddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+// export const nftaddress = '0x75770370B741053D7355E035b8D8BA399E30f6Cf'
+// export const nftmarketaddress = '0xed25353b184417083FB79a78236CaE1b171896EC'
+
+
+export const nftaddress = '0x1f81C1CF8aD39Dd63df186169000A727127537C7'
+export const nftmarketaddress = '0x7c3Da1675362Cfb05c2E0242e6474Cc806b77753'
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,9 +8,13 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
-      accounts: [privateKey],
+    // polygon_mumbai: {
+    //   url: 'https://polygon-mumbai.g.alchemy.com/v2/plHg9aNM4D0JqA0uTGx6sZblE-Qe6vmr',
+    //   accounts: ['0x9643cd7e54abc73c2faafa658c78b01323cfa87c7aa29eef40732270c72c74a0'],
+    // },
+    ropsten: {
+      url: 'https://ropsten.infura.io/v3/c695f583e2df43eaacd2d33ad9644143',
+      accounts: ['9643cd7e54abc73c2faafa658c78b01323cfa87c7aa29eef40732270c72c74a0'],
     },
   },
   solidity: "0.8.4",
