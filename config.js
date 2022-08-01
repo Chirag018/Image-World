7a483196d2a48f6e030ade124aec3521e477a15d
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-08-01 21:12:12 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// export const nftaddress = '0x75770370B741053D7355E035b8D8BA399E30f6Cf'
-// export const nftmarketaddress = '0xed25353b184417083FB79a78236CaE1b171896EC'
-
-
-export const nftaddress = '0xEA33776b4BC18650876AE7674745D2E82d55fc35'
-export const nftmarketaddress = '0xacE6897B779e25e2589c29b7AF9e3539c9545cf9'
